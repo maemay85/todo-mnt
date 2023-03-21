c17ce8ce7d6ce587fb6ed4f44aedd9277e2bc551
maemay85 <hellomaemay@gmail.com>
2023-03-20 20:49:09 -0400

    halfway through step 2 create our api

--- src/pages/api/todos/index.ts
+++ src/pages/api/todos/index.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 import { connect } from "../../../../utils/connection";
 import { ResponseFuncs } from "../../../../utils/types";
@@ -6,6 +7,27 @@ const handler =async (req: NextApiRequest, res: NextApiResponse) => {
   //capture request method, we type it as a key of ResponseFunc to reduce typing later
   const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
 
+  // catch errors
+  const catcher = (error: Error) => res.status(400).json({ error });
 
+  // Potential Responses
+  const handleCase: ResponseFuncs = {
+    // GET REQUESTS
+    GET: async (req: NextApiRequest, res: NextApiResponse) => {
+      const { Todo } = await connect() // connect to db
+      res.json(await Todo.find({}).catch(catcher))
+    },
+    // POST REQUESTS
+    POST: async (req: NextApiRequest, res: NextApiResponse) => {
+      const { Todo } = await connect() // connect to db
+      res.json(await Todo.create({}).catch(catcher))
+    },
+  }
 
+  // Check if there is a response for the particular method
+  const response = handleCase[method]
+  if (response) response(req, res)
+  else res.status(400).json({ error: "No Response for This Request" })
 }
+
+export default handler

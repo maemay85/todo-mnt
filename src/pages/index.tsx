6ffa5e7cfeb96716fd726b8dde47f868e7b08904
maemay85 <hellomaemay@gmail.com>
2023-03-21 18:21:25 -0400

    getServerSide props in pages/index.ts is failing - need to keep debugging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
 import { Todo } from "../../utils/types"
 import Link from "next/link"
 
-// Define the component props
+// Define the components props
 interface IndexProps {
   todos: Array<Todo>
 }
 
-//define the page component
+// define the page component
 function Index(props: IndexProps) {
   const { todos } = props
 
   return (
     <div>
       <h1>My Todo List</h1>
-      <h2>Click on Todo to see it individually</h2>
-      <Link href="/todos/create"><button>Create a New Todo</button></Link>
-      {/* MAPPING OVER TODOS */}
-      {todos.map(t=>(
+      <h2>Click On Todo to see it individually</h2>
+      {/* MAPPING OVER THE TODOS */}
+      {todos.map(t => (
         <div key={t._id}>
           <Link href={`/todos/${t._id}`}>
             <h3 style={{ cursor: "pointer" }}>
@@ -30,12 +29,12 @@ function Index(props: IndexProps) {
 }
 
 // GET PROPS FOR SERVER SIDE RENDERING
-export async function getServerSideProps(context: any) {
+export async function getServerSideProps() {
   // get todo data from API
   const res = await fetch(process.env.API_URL as string)
   const todos = await res.json()
 
-  //return props
+  // return props
   return {
     props: { todos },
   }
